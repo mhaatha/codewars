761498a55c8c72abf45454b7c3b8af07de2cd8ee
Hafidz Athaya <mhafidza528@gmail.com>
2026-05-11 08:46:11 +0800

    feat: [WIP] current implementation but not complete yet

--- 7kyu/rotate_for_a_max/rotate_for_a_max.go
+++ 7kyu/rotate_for_a_max/rotate_for_a_max.go
@@ -1,7 +1,28 @@
 package kata
 
+import (
+	"slices"
+	"strconv"
+	"strings"
+)
+
 // https://www.codewars.com/kata/56a4872cbb65f3a610000026/train/go
 
 func MaxRot(n int) int {
-	return 68957
+	splittedN := strings.Split(strconv.Itoa(n), "")
+	biggest, _ := strconv.Atoi(strings.Join(splittedN, ""))
+
+	for i := range splittedN {
+		if i == len(splittedN)-1 {
+			break
+		}
+		splittedN = append(splittedN, splittedN[i])
+		splittedN = slices.Delete(splittedN, i, i+1)
+		currentValue, _ := strconv.Atoi(strings.Join(splittedN, ""))
+		if biggest < currentValue {
+			biggest = currentValue
+		}
+	}
+
+	return biggest
 }
