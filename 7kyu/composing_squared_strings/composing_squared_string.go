d446e579db5d4083f15b8ae42522e1218921efcc
Hafidz Athaya <mhafidza528@gmail.com>
2026-05-18 07:35:55 +0800

    feat: [WIP] current implementation but not complete yet

--- 7kyu/composing_squared_strings/composing_squared_string.go
+++ 7kyu/composing_squared_strings/composing_squared_string.go
@@ -1,6 +1,7 @@
 package kata
 
 import (
+	"fmt"
 	"strings"
 )
 
@@ -11,6 +12,26 @@ func Compose(s1, s2 string) string {
 
 	splittedS1 := strings.Split(s1, "\n")
 	splittedS2 := strings.Split(s2, "\n")
+
+	s1Pointer := 1
+	s2Pointer := len(splittedS2)
+
+	for i := 0; i < len(splittedS1); i++ {
+		currentWord := ""
+
+		for j := 0; j < i+1; j++ {
+			currentWord += string(splittedS1[i][j])
+		}
+
+		for k := len(splittedS2) - (i + 1); len(splittedS2) > (len(splittedS2) - i); k-- {
+			fmt.Println(splittedS2[k])
+		}
+
+		strng += "\n"
+
+		s1Pointer++
+		s2Pointer--
+	}
 
 	return strng
 }
